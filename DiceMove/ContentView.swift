dc86ce7bcccd7e107c1816778accbed49f57393b
James Childress <james.childress@icloud>
2025-07-10 15:24:12 -0700

    Initial Commit

---
+++ DiceMove/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DiceMove
+//
+//  Created by James Childress on 7/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DiceMove/DiceMoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceMoveApp.swift
+//  DiceMove
+//
+//  Created by James Childress on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct DiceMoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
