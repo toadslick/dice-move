338dbc35884439d79a948b848c29cc83561d686a
James Childress <james.childress@icloud>
2025-07-11 14:52:51 -0700

    added asset sources to repo

--- DiceMove/DieView/DieView.swift
+++ DiceMove/DieView/DieView.swift
@@ -22,6 +22,6 @@ struct DieView: UIViewControllerRepresentable, DieController.Delegate {
     }
     
     func dieDidStopAtValue(_ value: Int) {
-        
+        print("You rolled a \(value)")
     }
 }
